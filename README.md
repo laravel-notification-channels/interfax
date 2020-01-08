06432af8111afa2cf870c7ed40b4e473b0fae6b4
iv-craig <cs@ivinteractive.com>
2020-01-08 16:42:02 -0500

    Add InterFAX notifications channel

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
-# interfax
-InterFAX notification channel for Laravel
+# InterFAX notification channel for Laravel 5.8+ and 6.x
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/laravel-notification-channels/interfax.svg?style=flat-square)](https://packagist.org/packages/laravel-notification-channels/interfax)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+[![Build Status](https://img.shields.io/travis/laravel-notification-channels/interfax/master.svg?style=flat-square)](https://travis-ci.org/laravel-notification-channels/interfax)
+[![StyleCI](https://styleci.io/repos/217342993/shield)](https://styleci.io/repos/217342993)
+[![SensioLabsInsight](https://img.shields.io/sensiolabs/i/5c642897-a39d-4a62-8618-2880d818b101.svg?style=flat-square)](https://insight.sensiolabs.com/projects/5c642897-a39d-4a62-8618-2880d818b101)
+[![Quality Score](https://img.shields.io/scrutinizer/g/laravel-notification-channels/interfax.svg?style=flat-square)](https://scrutinizer-ci.com/g/laravel-notification-channels/interfax)
+[![Code Coverage](https://img.shields.io/scrutinizer/coverage/g/laravel-notification-channels/interfax/master.svg?style=flat-square)](https://scrutinizer-ci.com/g/laravel-notification-channels/interfax/?branch=master)
+[![Total Downloads](https://img.shields.io/packagist/dt/laravel-notification-channels/interfax.svg?style=flat-square)](https://packagist.org/packages/laravel-notification-channels/interfax)
+
+This package makes it easy to send notifications using [InterFAX](https://interfax.net) with Laravel 5.8+ and 6.x.
+
+## Contents
+
+- [Installation](#installation)
+  - [Setting up the InterFAX service](#setting-up-the-InterFAX-service)
+- [Usage](#usage)
+  - [Available Message methods](#available-message-methods)
+- [Changelog](#changelog)
+- [Testing](#testing)
+- [Security](#security)
+- [Contributing](#contributing)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation
+
+You can install this package via composer:
+
+```bash
+composer require laravel-notification-channels/interfax
+```
+
+The service provider gets loaded automatically.
+
+### Setting up the InterFAX service
+
+This channel will use your InterFAX username and password. To use the channel, add this to your `config/services.php` file:
+
+```php
+...
+'interfax' => [
+  'username' => env('INTERFAX_USERNAME'),
+  'password' => env('INTERFAX_PASSWORD'),
+  'pci'      => env('INTERFAX_PCI', false),
+],
+...
+```
+
+This will load your InterFAX credentials from the `.env` file. If your requests must be PCI-DSS-compliant, set `INTERFAX_PCI=true` in your `.env` file.
+
+## Usage
+
+To use this package, you can create a notification class, like `DocumentWasSent` from the example below, in your Laravel application. Make sure to check out [Laravel's documentation](https://laravel.com/docs/master/notifications) for this process.
+
+### Send PDF via fax
+
+```php
+<?php
+use NotificationChannels\Interfax\InterfaxChannel;
+use NotificationChannels\Interfax\InterfaxMessage;
+
+class DocumentWasSent extends Notification
+{
+
+    protected $files;
+
+    public function __construct(array $files)
+    {
+    $this->files = $files;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [InterfaxChannel::class];
+    }
+
+    public function toInterfax($notifiable)
+    {
+        return (new InterfaxMessage)
+              ->files($this->files);
+    }
+}
+```
+
+The Notifiable model will need to return a destination fax number.
+
+```php
+public function routeNotificationForInterfax($notification)
+{
+    if($this->fax)
+        return preg_replace('/[^\d]/', '', $this->fax);
+
+    return null;
+}
+```
+
+### Available Message methods
+
+`file(string $file)` : Accepts the full path to a single file (full list of supported file types [found here](https://www.interfax.net/en/help/supported_file_types)).  
+`files(array $array)` : Accepts an array of file paths.  
+`stream(Filestream $stream, string $name)` : Accepts a file stream.
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Security
+
+If you discover any security related issues, please email craig.spivack@ivinteractive.com instead of using the issue tracker.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Credits
+
+- [Craig Spivack](https://github.com/iv-craig)
+- [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ tests/InterfaxChannelTest.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace NotificationChannels\Interfax\Test;
+
+use Illuminate\Notifications\Notification;
+use Interfax\Client as InterfaxClient;
+use Interfax\Resource as InterfaxResource;
+use Mockery;
+use NotificationChannels\Interfax\Exceptions\CouldNotSendNotification;
+use NotificationChannels\Interfax\InterfaxChannel;
+use NotificationChannels\Interfax\InterfaxMessage;
+
+class InterfaxChannelTest extends TestCase
+{
+    /** @var Mockery\Mock */
+    protected $interfax;
+
+    /** @var Mockery\Mock */
+    protected $resource;
+
+    /** @var \NotificationChannels\Interfax\InterfaxChannel */
+    protected $channel;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config([
+            'services.interfax.interval' => 1,
+        ]);
+
+        $this->interfax = Mockery::mock(InterfaxClient::class);
+        $this->resource = Mockery::mock(InterfaxResource::class);
+        $this->channel = new InterfaxChannel($this->interfax);
+    }
+
+    /** @test */
+    public function it_can_send_notification_with_a_single_file()
+    {
+        $this->interfax->shouldReceive('deliver')
+            ->once()
+            ->with([
+                'faxNumber' => '12345678901',
+                'files' => ['test-file.pdf'],
+            ]);
+
+        $this->channel->send(new TestNotifiable, new TestNotificationWithSingleFile);
+    }
+
+    /** @test */
+    public function it_can_send_notification_with_files()
+    {
+        $this->interfax->shouldReceive('deliver')
+            ->once()
+            ->with([
+                'faxNumber' => '12345678901',
+                'files' => ['test-file-1.pdf', 'test-file-2.pdf'],
+            ]);
+
+        $this->channel->send(new TestNotifiable, new TestNotificationWithFiles);
+    }
+
+    /** @test */
+    public function it_can_send_notification_pdf_as_stream()
+    {
+        $this->interfax->shouldReceive('deliver')
+            ->with(Mockery::on(function ($output) {
+                if ($output['faxNumber'] !== '12345678901') {
+                    return false;
+                }
+
+                $file = $output['files'][0];
+
+                if ($file[1]['name'] !== app('filesystem')->path('test-file.pdf')) {
+                    return false;
+                }
+
+                if ($file[1]['mime_type'] !== 'application/pdf') {
+                    return false;
+                }
+
+                if (! is_resource($file[0])) {
+                    return false;
+                }
+
+                return true;
+            }));
+
+        $this->channel->send(new TestNotifiable, new TestNotificationAsStreamPdf);
+    }
+
+    /** @test */
+    public function it_can_send_notification_html_as_stream()
+    {
+        $filename = 'test-file.html';
+        $this->addFile($filename);
+
+        app('filesystem')->put($filename, '<html><body><h1>Test file contents</h1></body></html>');
+
+        $this->interfax->shouldReceive('deliver')
+            ->with(Mockery::on(function ($output) {
+                if ($output['faxNumber'] !== '12345678901') {
+                    return false;
+                }
+
+                $file = $output['files'][0];
+
+                if ($file[1]['name'] !== app('filesystem')->path('test-file.html')) {
+                    return false;
+                }
+
+                if ($file[1]['mime_type'] !== 'text/html') {
+                    return false;
+                }
+
+                if (! is_resource($file[0])) {
+                    return false;
+                }
+
+                return true;
+            }));
+
+        $this->channel->send(new TestNotifiable, new TestNotificationAsStreamHtml);
+    }
+
+    /** @test */
+    public function it_can_return_early_when_no_fax_number_provided()
+    {
+        $this->assertNull($this->channel->send(new TestNotifiableNotSendable, new TestNotificationWithFiles));
+    }
+
+    /** @test */
+    public function it_can_refresh_the_file_response()
+    {
+        $this->resource
+             ->shouldReceive('refresh')
+             ->andReturn((object) [
+                 'status' => -1,
+             ], (object) [
+                 'status' => 0,
+             ]);
+
+        $this->interfax
+             ->shouldReceive('deliver')
+             ->andReturn($this->resource);
+
+        $this->channel->send(new TestNotifiable, new TestNotificationWithRefresh);
+    }
+
+    /** @test */
+    public function it_can_throw_the_exception()
+    {
+        $this->expectException(CouldNotSendNotification::class);
+
+        $this->resource
+             ->shouldReceive('refresh')
+             ->andReturn(new TestResource);
+
+        $this->interfax
+             ->shouldReceive('deliver')
+             ->andReturn($this->resource);
+
+        $this->channel->send(new TestNotifiable, new TestNotificationWithRefresh);
+    }
+}
+
+class TestNotificationWithSingleFile extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return InterfaxMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toInterfax($notifiable)
+    {
+        return (new InterfaxMessage)
+                    ->user($notifiable)
+                    ->file('test-file.pdf');
+    }
+}
+
+class TestNotificationWithFiles extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return InterfaxMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toInterfax($notifiable)
+    {
+        return (new InterfaxMessage)
+                    ->user($notifiable)
+                    ->files(['test-file-1.pdf', 'test-file-2.pdf']);
+    }
+}
+
+class TestNotificationAsStreamPdf extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return InterfaxMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toInterfax($notifiable)
+    {
+        $path = app('filesystem')->path('test-file.pdf');
+
+        return (new InterfaxMessage)
+                    ->user($notifiable)
+                    ->stream(fopen($path, 'r'), $path);
+    }
+}
+
+class TestNotificationAsStreamHtml extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return InterfaxMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toInterfax($notifiable)
+    {
+        $path = app('filesystem')->path('test-file.html');
+
+        return (new InterfaxMessage)
+                    ->user($notifiable)
+                    ->stream(fopen($path, 'r'), $path);
+    }
+}
+
+class TestNotificationWithRefresh extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return InterfaxMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toInterfax($notifiable)
+    {
+        return (new InterfaxMessage)
+                    ->checkStatus()
+                    ->user($notifiable)
+                    ->files(['test-file-1.pdf', 'test-file-2.pdf']);
+    }
+}
+
+class TestResource
+{
+    public $status = 500;
+
+    public function attributes()
+    {
+        return [
+            'status' => $this->status,
+            'message' => 'Failed to send.',
+        ];
+    }
+}
