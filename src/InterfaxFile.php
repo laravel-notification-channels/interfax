e47169ea79820b25d64d6ae4c27d95f65ac3994b
iv-craig <craig.spivack@iv.agency>
2024-12-16 15:53:28 -0500

    Address static analysis issues for PHP 8.4

--- src/InterfaxFile.php
+++ src/InterfaxFile.php
@@ -7,12 +7,13 @@ class InterfaxFile extends \Interfax\File
     /**
      * File constructor.
      *
+     * @param  \Interfax\Client $client
      * @param  resource|string $location
      * @param  array<string, mixed>  $params
      *
      * @throws \InvalidArgumentException
      */
-    public function __construct(\Interfax\Client $client, $location, $params = [], \Interfax\GenericFactory $factory = null)
+    public function __construct(\Interfax\Client $client, $location, $params = [], ?\Interfax\GenericFactory $factory = null)
     {
         if ($chunkSize = config('services.interfax.chunk_size')) {
             static::$DEFAULT_CHUNK_SIZE = $chunkSize;
